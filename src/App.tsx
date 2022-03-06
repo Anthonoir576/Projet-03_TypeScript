d5a866c949f42d32136a7cc0652c67011682d975
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-03-06 13:11:40 +0100

    :FEAT:  En cours de finition

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,75 @@
 
 import React        from 'react';
 import { apiKey }   from './Components/Api/apiKey';
-import Location     from './Components/Location/Location';
-import Meteo        from './Components/Meteo/Meteo';
-import Search       from './Components/Search/Search';
+// import Location  from './Components/Location/Location';
+// import Meteo     from './Components/Meteo/Meteo';
+// import Search    from './Components/Search/Search';
 import { useState } from 'react';
+import axios        from 'axios';
 
 
 
 function App() {
   
 
-      // Fonction
-      const dateFonction = (dateDay?: any) => {      
+  // Fonction
+  const dateFonction = (dateDay?: any) => {      
 
-        let mois    : string[]   = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        let jours   : string []  = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    
-        let jour    = jours[new Date().getDate()];
-        console.log(jour);
-        console.log('-------');
+    let mois    : string[]   = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let jours   : string []  = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-        let date    = new Date().getDate();
-        console.log(date);
-        console.log('-------');
+    let jour    = jours[new Date().getDate()];
+    // console.log(jour);
+    // console.log('-------');
 
-        let month  = mois[new Date().getMonth()];
-        console.log(month);
-        console.log('-------');
+    let date    = new Date().getDate();
+    // console.log(date);
+    // console.log('-------');
 
-        let annee  = new Date().getFullYear();
-        console.log(annee);
-        console.log('-------');
+    let month  = mois[new Date().getMonth()];
+    // console.log(month);
+    // console.log('-------');
 
-        return `${jour} ${date} ${month} ${annee}`;
+    let annee  = new Date().getFullYear();
+    // console.log(annee);
+    // console.log('-------');
+
+    return `${jour} ${date} ${month} ${annee}`;
+  };
+
+  const [request, setRequest]   = useState('');
+  const [meteo, setMeteo]       = useState({});
+  
+
+  const search = (e: any) => {
+
+    if (e.key === "Enter") {
+ 
+      axios({        
+        method: 'GET',
+        url: `${apiKey.base}weather?q=${request}&units=metric&APPID=${apiKey.key}`
+      })
+      .then((result :any) => {
+        console.log('Saisi réussie !');
+        // console.log(meteo);
+        setRequest('');
+        result.json();     
+      
+      }).then((result :any) => {
+        console.log('Saisi réussie !');
+        // console.log(meteo);
+        setRequest(''); 
+        setMeteo(result);
+
+      }).catch((err   :any) => {
+        setRequest('');    
+        // console.log(err);
+      });
     };
 
-    const [request, setRequest]   = useState('');
-    const [meteo, setMeteo]       = useState({});
+  };
+
+
 
   return (
     <div className="App">
@@ -46,7 +78,10 @@ function App() {
         <div className="recherche">
             <input type="text" 
                   className="recherche-bar"
-                  placeholder='  Recherche...' 
+                  placeholder='  Recherche...'
+                  onChange={(e :any) => {setRequest(e.target.value);}}
+                  value={request}
+                  onKeyPress={search} 
             />
           </div>
 
